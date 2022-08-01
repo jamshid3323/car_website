086b0cc214f5fa650f280947f9a781cb84b24d42
Jamshid <jamshidtolibjonov3323@gmail.com>
2022-08-01 10:22:43 +0500

    users app added and connect to main urls

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('user/', include('users.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
